d85a03b7611a335e4e3732fcb3ff0946430149c8
vladikius <vladimir.mamani@outlook.com>
2016-10-08 18:38:00 -0500

    Update accesosControler.php

--- accesosControler.php
+++ accesosControler.php
@@ -1,16 +1,11 @@
 <?PHP  if ( ! defined('BASEPATH'))  exit('No direct script access allowed');
-
 include( "../customfunctions.php" );
-
-
 class accesosControler extends CI_Controller {
-
     function __construct()
     {
         parent::__construct();
         $this->load->model('obtDatos');
     }
-
     //--------------------------------------------------------------
     // Area de Verifificación de usuarios
     //--------------------------------------------------------------
@@ -18,30 +13,26 @@ class accesosControler extends CI_Controller {
     {
 		$usuario = getPostForm( "usuario" );
 		$password = getPostForm( "password" );
-
-		$pass = conviertePassword( $pass );  sin cifrar
-
-		if( $row=$this->ado->obtDatos("posJurados", "DNI='$user' AND Clave='$pass'") ) {
-
+		
+		$pass = conviertePassword( $pass );  //sin cifrar
+		if( $row=$this->ado->obtDatos("posJurados", "DNI='$user' AND Clave='$pass'")) 
+		{
 			$array = array (
 				'id'   => $row->id,
 				'user' => $row->ape,
 				'area' => $row->area,
 				'tipo' => $row->tip,
 			);
-
         	$this->session->set_userdata('logged_in', $array);
 		}
-
-		  redirect( base_url(""), 'refresh');
+		redirect( base_url(""), 'refresh');
     }
 	public function salirCalificador()
     {
         $this->session->sess_destroy();
         redirect( base_url("jurado"));
     }
-
-public function verificaAcceso()
+	public function verificaAcceso()
     {
         $data = $this->session->userdata('logged_in');
         // Fuera si no hay datos de session
