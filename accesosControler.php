a7e732b9c00cdded6ea6e7be640b156a86736b65
vladikius <vladimir.mamani@outlook.com>
2016-10-08 18:22:50 -0500

    Update accesosControler.php

--- accesosControler.php
+++ accesosControler.php
@@ -15,24 +15,35 @@ class accesosControler extends CI_Controller {
     // Area de Verifificación de usuarios
     //--------------------------------------------------------------
     public function login()
+		{
+			$usuario = getPostForm( "usuario" );
+			$password = getPostForm( "password" );
+
+			$pass = conviertePassword( $pass );  //sin cifrar
+
+			if( $row=$this->ado->obtDatos("posJurados", "DNI='$user' AND Clave='$pass'") ) {
+				$array = array (
+					'id'   => $row->id,
+					'user' => $row->ape,
+					'area' => $row->area,
+					'tipo' => $row->tip,
+				);
+						$this->session->set_userdata('logged_in', $array);
+			}
+			redirect( base_url(""), 'refresh');
+    }
+		public function salirCalificador()
     {
-		$usuario = getPostForm( "usuario" );
-		$password = getPostForm( "password" );
+			$this->session->sess_destroy();
+			redirect( base_url("jurado"));
+	  }
 
-		$pass = conviertePassword( $pass );  sin cifrar
-
-		if( $row=$this->ado->obtDatos("posJurados", "DNI='$user' AND Clave='$pass'") ) {
-
-			$array = array (
-				'id'   => $row->id,
-				'user' => $row->ape,
-				'area' => $row->area,
-				'tipo' => $row->tip,
-			);
-
-        	$this->session->set_userdata('logged_in', $array);
-		}
-
-		  redirect( base_url(""), 'refresh');
+		public function verificaAcceso()
+    {
+        $data = $this->session->userdata('logged_in');
+        // Fuera si no hay datos de session
+        if( $data == null ) $this->logout();
+        // Retorna datos de session si esta adentro
+        return $data;
     }
 }
